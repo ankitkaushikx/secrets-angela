5791eba36cace95695d549f2ad7dc1a47337d5fd
Ankit Kaushik <ankitkaushikx@gmail.com>
2023-11-20 12:24:03 +0530

    OAuth registration and login with google added - EWF

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 import express from "express";
-import ejs from "ejs";
+// import ejs from "ejs";
 import session from "express-session";
 import passport from "passport";
 import passportLocalMongoose from "passport-local-mongoose";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import findOrCreate from "mongoose-findorcreate";
+
 // ?------------------------------------------------MIDDLEWARES----///////////////////////////////////
 
 const app = express();
-app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 // ?------------------------------------------------CONNECTIONS----/////////////////////////////////////////////////////////////////
 app.use(
@@ -31,16 +35,59 @@ mongoose.connect(uri);
 const ListSchema = new mongoose.Schema({
   username: String,
   password: String,
+  googleId: String,
 });
 
 ListSchema.plugin(passportLocalMongoose);
 const List = new mongoose.model("list", ListSchema);
 ListSchema.plugin(passportLocalMongoose);
 
-passport.use(List.createStrategy());
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
 
-passport.serializeUser(List.serializeUser());
-passport.deserializeUser(List.deserializeUser());
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+ListSchema.plugin(findOrCreate);
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+      try {
+        console.log(profile);
+        // Try to find a user with the Google ID in the database
+        const existingUser = await List.findOne({ googleid: profile.id });
+
+        if (existingUser) {
+          // If user exists, return the user
+          return cb(null, existingUser);
+        } else {
+          // If user doesn't exist, create a new user
+          const newUser = new List({
+            // Adjust as needed
+            googleId: profile.id,
+            username: profile.displayName,
+            // Add other fields as needed
+          });
+
+          // Save the new user to the database
+          await newUser.save();
+
+          // Return the newly created user
+          return cb(null, newUser);
+        }
+      } catch (error) {
+        return cb(error, null);
+      }
+    }
+  )
+);
 
 // !---------------------------------------------------ROUTES---///////////////////////////////////////////////////////////////////
 app.get("/", async (req, res) => {
@@ -73,6 +120,12 @@ app.get("/secrets", function (req, res) {
   } else {
     res.redirect("/login");
   }
+});
+
+app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] }));
+
+app.get("/auth/google/secrets", passport.authenticate("google", { failureRedirect: "/login" }), function (req, res) {
+  res.redirect("/secrets");
 });
 
 app.post("/register", async (req, res) => {
